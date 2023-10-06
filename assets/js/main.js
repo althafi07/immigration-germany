5908bf166595c3f5c5cf867f37a3f803497b0836
Althaf <althafi07@gmail.com>
2023-10-06 17:56:08 +0200

    Enhanced content

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,12 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function toggleMenu() {
+    var navLinks = document.getElementById("navLinks");
+    if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
+    } else {
+        navLinks.style.display = "block";
+    }
+}
